3e281d8522f8da5da1efd7b29cf1a29d185ed723
João Vinaud <leyenasd@gmail.com>
2020-05-02 19:14:48 -0300

    ajuste botão

--- src/components/BtnWhatsapp.js
+++ src/components/BtnWhatsapp.js
@@ -6,7 +6,7 @@ const BtnWhatsapp = () => {
         <div className='bg-white shadow-2xl rounded px-5 py-2 flex flex-col items-center mt-6 sm:mt-0 sm:flex-row'>
                <img className="object-scale-down h-20 w-20"  src={wa} alt='Whatsapp'/>
                <div className='ml-2'>
-                   <h3 className='font-bold text-2xl'>Orçar seu projeto</h3>
+                   <h3 className='font-bold text-2xl'>Orçar o seu projeto</h3>
                    <p className='text-xs'>Projeto e orçamento sem compromisso</p>
                    <p className='font-bold'>(84) 91234-5678</p>
                </div>
