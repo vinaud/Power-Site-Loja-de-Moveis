b5aa3b9be778355267ac6413ded378a835049f2f
João Vinaud <leyenasd@gmail.com>
2020-05-02 11:55:02 -0300

    configuração

---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\João Vinaud\\Documents\\Projetos\\Power Sites\\Loja Moveis\\.cache\\dev-404-page.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\João Vinaud\\Documents\\Projetos\\Power Sites\\Loja Moveis\\src\\pages\\index.js")))
+}
+
